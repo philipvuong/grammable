6ef445fbfe0042848ee7bdc9f0720a038fad9d15
Philip Vuong <philipvuong@yahoo.com>
2018-09-16 23:45:25 +0000

    Destroying grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -30,15 +30,25 @@ class GramsController < ApplicationController
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-
     @gram.update_attributes(gram_params)
-
     if @gram.valid?
       redirect_to root_path
     else
       return render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
+
+
+
+
 
   private
 
